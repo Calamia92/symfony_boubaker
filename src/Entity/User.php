a4f11810c8ec0bb77dffad812aa28e9770a05900
DigitalDevFactory <119439868+Calamia92@users.noreply.github.com>
2024-02-16 15:29:24 +0100

    sasa

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,90 +3,82 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`user`')]
-class User
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $name = null;
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $surname = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $mail = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $password = null;
-
-    #[ORM\Column(type: 'json')]
+    #[ORM\Column]
     private array $roles = [];
 
-
-    #[ORM\OneToMany(targetEntity: Article::class, mappedBy: 'author')]
-    private Collection $articles;
-
-    #[ORM\OneToMany(targetEntity: Comments::class, mappedBy: 'author', orphanRemoval: true)]
-    private Collection $comments;
-
-    public function __construct()
-    {
-        $this->comments = new ArrayCollection();
-        $this->articles = new ArrayCollection();
-
-    }
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getName(): ?string
+    public function getEmail(): ?string
     {
-        return $this->name;
+        return $this->email;
     }
 
-    public function setName(string $name): static
+    public function setEmail(string $email): static
     {
-        $this->name = $name;
+        $this->email = $email;
 
         return $this;
     }
 
-    public function getSurname(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
     {
-        return $this->surname;
+        return (string) $this->email;
     }
 
-    public function setSurname(string $surname): static
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
     {
-        $this->surname = $surname;
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
 
         return $this;
     }
 
-    public function getMail(): ?string
-    {
-        return $this->mail;
-    }
-
-    public function setMail(string $mail): static
-    {
-        $this->mail = $mail;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -98,79 +90,12 @@ class User
         return $this;
     }
 
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        if (empty($roles)) {
-            $roles[] = 'ROLE_USER';
-        }
-    
-        return array_unique($roles);
-    }
-    
-    public function setRoles(array $roles): self
-    {
-        $this->roles = $roles;
-    
-        return $this;
-    }
-    
     /**
-     * @return Collection<int, Comments>
+     * @see UserInterface
      */
-    public function getComments(): Collection
+    public function eraseCredentials(): void
     {
-        return $this->comments;
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
-
-    public function addComment(Comments $comment): static
-    {
-        if (!$this->comments->contains($comment)) {
-            $this->comments->add($comment);
-            $comment->setAuthor($this);
-        }
-
-        return $this;
-    }
-
-    public function removeComment(Comments $comment): static
-    {
-        if ($this->comments->removeElement($comment)) {
-            // set the owning side to null (unless already changed)
-            if ($comment->getAuthor() === $this) {
-                $comment->setAuthor(null);
-            }
-        }
-
-        return $this;
-    }
-    public function addArticle(Article $article): self
-{
-    if (!$this->articles->contains($article)) {
-        $this->articles[] = $article;
-        $article->setAuthor($this);
-    }
-
-    return $this;
-}
-
-public function removeArticle(Article $article): self
-{
-    if ($this->articles->removeElement($article)) {
-        // Set the owning side to null (unless it's already changed)
-        if ($article->getAuthor() === $this) {
-            $article->setAuthor(null);
-        }
-    }
-
-    return $this;
-}
-
-/**
- * @return Collection|Article[]
- */
-public function getArticles(): Collection
-{
-    return $this->articles;
-}
 }
